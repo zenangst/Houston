4c9cab5fc8b3ef20d1b693ad8c1d16e50709d988
Christoffer Winterkvist <christoffer@winterkvist.com>
2021-11-24 07:53:54 +0100

    Fix realignment being called twice
    - Remove timer when invoking MissionControl.realign and re-add the timer
      when the operation is done to ensure that we don't trigger realignment
      while the process is already running.

--- Sources/MissionControl.swift
+++ Sources/MissionControl.swift
@@ -1,21 +1,26 @@
 import Cocoa
 
 enum MissionControl {
+  typealias CompletionHandler = (() -> Void)?
   /// Open the Mission Control application
   /// - Parameter handler: A completion closure that is called
   ///                      directly after the open application invocation
   ///                      occurred.
-  static func open(then handler: (() -> Void)? = nil) {
+  static func open(then handler: CompletionHandler = nil) {
     let path = "/System/Applications/Mission Control.app"
     NSWorkspace.shared.open(URL(fileURLWithPath: path))
     handler?()
   }
 
-  static func realign() {
+  static func realign(then handler: CompletionHandler = nil) {
     MissionControl.open {
       // TODO: Should this be a preference that you can toggle on and off?
       let deadline = DispatchTime.now() + NSAnimationContext.current.duration + 0.035
-      DispatchQueue.main.asyncAfter(deadline: deadline) { MissionControl.open() }
+      DispatchQueue.main.asyncAfter(deadline: deadline) {
+        MissionControl.open {
+          handler?()
+        }
+      }
     }
   }
 }
